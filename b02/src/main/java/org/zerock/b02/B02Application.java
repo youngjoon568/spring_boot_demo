558f95b397440bd5fb34055bebea0a2f1031f395
YoungJoon <93526791+youngjoon568@users.noreply.github.com>
2024-04-25 14:15:46 +0900

    save

---
+++ b02/src/main/java/org/zerock/b02/B02Application.java
@@ -0,0 +1,13 @@
+package org.zerock.b02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B02Application.class, args);
+	}
+
+}
---
+++ b02/src/main/java/org/zerock/b02/controllers/DemoController.java
@@ -0,0 +1,5 @@
+package org.zerock.b02.controllers;
+
+public class DemoController {
+    
+}
