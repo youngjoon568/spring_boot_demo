1b1077d0f66c700bcefe417b172a04c0a0d596f8
YoungJoon <93526791+youngjoon568@users.noreply.github.com>
2024-04-25 15:15:41 +0900

    save

--- b01/src/main/java/org/zerock/b01/controllers/SampleController.java
+++ b01/src/main/java/org/zerock/b01/controllers/SampleController.java
@@ -3,6 +3,8 @@ import org.springframework.stereotype.Controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @Log4j2
@@ -13,8 +15,17 @@ public class SampleController {
     public void hello(Model model) {
 
         log.info("hello...............");
-        // model.addAttribute("msg","Hello, World");
-        model.addAttribute("msg", "Hello, Spring");
+        model.addAttribute("msg","Hello, World");
+    }
+
+    @GetMapping("ex/ex1")
+    public void ex1(Model model) {
+        List<String> list = new ArrayList<>();
+        list.add("AAA");
+        list.add("BBB");
+        list.add("CCC");
+
+        model.addAttribute("list", list);
     }
     
 }
--- b01/src/main/java/org/zerock/b01/controllers/SampleJsonController.java
+++ b01/src/main/java/org/zerock/b01/controllers/SampleJsonController.java
@@ -1,9 +1,8 @@
 package org.zerock.b01.controllers;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import lombok.extern.log4j.Log4j2;
+
 
 @RestController
 @Log4j2
