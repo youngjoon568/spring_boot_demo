73991122f37a44dbf48c116285eea49befca00c1
YoungJoon <93526791+youngjoon568@users.noreply.github.com>
2024-05-01 11:16:42 +0900

    save

---
+++ b01/src/main/java/org/zerock/b01/domain/Reply.java
@@ -0,0 +1,32 @@
+package org.zerock.b01.domain;
+
+import groovy.transform.ToString;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(excludes = "board")
+public class Reply extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long rno;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Board board;
+
+    private String replyText;
+
+    private String replyer;
+}
